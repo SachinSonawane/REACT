fba259796e30019e18dadcbfada0a5166d3381fd
SachinSonawane <prakash.jat@xebia.com>
2020-01-21 10:55:24 +0530

    react setup done

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 class App extends Component{
+   setName(firstName, lastName) {
+      return firstName+' '+lastName;
+   }
+
    render(){
       return(
          <div>
-            <h1>Hello World</h1>
+            <h1>Hello {this.setName('sachin', 'sonawane')}</h1>
          </div>
       );
    }
